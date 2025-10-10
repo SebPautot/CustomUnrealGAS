4978558b9f70a72f3169d3b4718b32a6cb71db02
TjgL <lithmoneo@gmail.com>
2025-10-10 14:30:15 +0200

    Renamed ability system sub-folder to abilities

---
+++ Source/CustomUnrealGAS/Private/AbilitySystem/Abilities/ASAbilitySystem.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/ASAbilitySystem.h"
+
+
+UASAbilitySystem::UASAbilitySystem()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UASAbilitySystem::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
---
+++ Source/CustomUnrealGAS/Public/AbilitySystem/Abilities/ASAbilitySystem.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ASAbilitySystem.generated.h"
+
+
+UCLASS(ClassGroup=(AbilitySystem), meta=(BlueprintSpawnableComponent))
+class CUSTOMUNREALGAS_API UASAbilitySystem : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UASAbilitySystem();
+
+protected:
+	virtual void BeginPlay() override;
+};
