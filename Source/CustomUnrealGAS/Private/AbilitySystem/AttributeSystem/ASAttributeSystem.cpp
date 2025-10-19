8fe65275445662c65bff2491133277323d8820da
SebPautot <spautot.mgz@hotmail.com>
2025-10-19 14:58:26 +0200

    Added AttributeData, Attribute and Attribute System def and basic implementations

--- Source/CustomUnrealGAS/Private/AbilitySystem/AttributeSystem/ASAttributeSystem.cpp
+++ Source/CustomUnrealGAS/Private/AbilitySystem/AttributeSystem/ASAttributeSystem.cpp
@@ -3,3 +3,25 @@
 
 #include "AbilitySystem/AttributeSystem/ASAttributeSystem.h"
 
+float UASAttribute::GetValue()
+{
+    return 0.0f;
+}
+
+void UASAttribute::SetValue(float NewValue)
+{
+}
+
+void UASAttribute::SetLevel(int Level)
+{
+}
+
+UASAttribute * UASAttributeSystem::InstantiateAttributesFromData(const TArray<FAttributeData*>& AttributeArray)
+{
+    for(FAttributeData* Attr : AttributeArray)
+    {
+        UASAttribute* NewAttribute = NewObject<UASAttribute>();
+        NewAttribute->SetData(*Attr);
+        Attributes.Add(Attr->Name, NewAttribute);
+    }
+}
--- Source/CustomUnrealGAS/Public/AbilitySystem/AttributeSystem/ASAttributeSystem.h
+++ Source/CustomUnrealGAS/Public/AbilitySystem/AttributeSystem/ASAttributeSystem.h
@@ -6,11 +6,35 @@
 #include "UObject/NoExportTypes.h"
 #include "ASAttributeSystem.generated.h"
 
+USTRUCT()
+struct FAttributeData {
+	GENERATED_BODY()
+
+	FName Name;
+	FText DisplayName;
+};
+
+UCLASS()
+class UASAttribute : public UObject {
+	GENERATED_BODY()
+
+	FAttributeData Data;
+	float BaseValue;
+	float CurrentValue;
+public:
+	float GetValue();
+	void SetValue(float NewValue);
+	void SetLevel(int Level);
+	void SetData(FAttributeData NewData) { Data = NewData; }
+};
 /**
  * 
  */
-UCLASS()
-class CUSTOMUNREALGAS_API UASAttributeSystem : public UComponent
+UCLASS(BlueprintType)
+class CUSTOMUNREALGAS_API UASAttributeSystem : public UActorComponent
 {
 	GENERATED_BODY()
+
+	TMap<FName, TObjectPtr<UASAttribute>> Attributes;
+	UASAttribute *InstantiateAttributesFromData(const TArray<FAttributeData *> &AttributeArray);
 };
