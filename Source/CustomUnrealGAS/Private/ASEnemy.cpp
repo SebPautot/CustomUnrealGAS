9330bb1182c5bd24fa21187af2e9baed4ebbb857
TjgL <lithmoneo@gmail.com>
2025-10-26 11:07:45 +0100

    Created base class for enemy

---
+++ Source/CustomUnrealGAS/Private/ASEnemy.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ASEnemy.h"
+
+
+AASEnemy::AASEnemy()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AASEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/CustomUnrealGAS/Private/ASEnemy.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ASEnemy.generated.h"
+
+class UASAttributeSystem;
+
+UCLASS()
+class CUSTOMUNREALGAS_API AASEnemy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Attribute")
+	TObjectPtr<UASAttributeSystem> AttributeSystem;
+
+public:
+	AASEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+};
