11e2f220ef033ab987be7c639e627f2c2b41c1ab
TjgL <lithmoneo@gmail.com>
2025-10-31 09:31:56 +0100

    Created basic health component

---
+++ Source/CustomUnrealGAS/Private/ASHealthComponent.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ASHealthComponent.h"
+
+
+UASHealthComponent::UASHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UASHealthComponent::TakeDamage(int Damage)
+{
+	Health -= Damage;
+	if (Health <= 0)
+	{
+		OnDeath.Broadcast();
+	}
+}
+
+
+void UASHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
---
+++ Source/CustomUnrealGAS/Public/ASHealthComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ASHealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature)
+
+
+UCLASS(ClassGroup=(Health), meta=(BlueprintSpawnableComponent))
+class CUSTOMUNREALGAS_API UASHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+	/** The max health of the associated actor.	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = true, ClampMin = 0))
+	int MaxHealth = 100;
+
+	/** The current health of the actor. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = true, ClampMin = 0))
+	int Health = 100;
+
+public:
+	UASHealthComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void TakeDamage(int Damage);
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Health")
+	FOnDeathSignature OnDeath;
+
+protected:
+	virtual void BeginPlay() override;
+};
