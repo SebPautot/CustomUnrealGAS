38302cf01d522526b1f169e8cde750fa4d44d3cb
TjgL <lithmoneo@gmail.com>
2025-11-01 20:49:32 +0100

    Created fire strike ability

---
+++ Source/CustomUnrealGAS/Private/AbilitySystem/Abilities/ASFireStrike.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/ASFireStrike.h"
+
+#include "ASHealthComponent.h"
+#include "ASTargetable.h"
+#include "AbilitySystem/AttributeSystem/ASAttribute.h"
+#include "AbilitySystem/AttributeSystem/ASAttributeSystem.h"
+
+void UASFireStrike::UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor)
+{
+	IASTargetable* Target {TargetableActor.GetInterface()};
+	const auto AttributeSystem = Target->GetAttributeSystem();
+	if (const auto BurnAttribute = AttributeSystem->TryGetAttribute(GetGenericData()))
+	{
+		const auto Health = Target->TryGetHealthComponent();
+		if (Health == nullptr)
+		{
+			GEngine->AddOnScreenDebugMessage(100, 5.f, FColor::Red, TEXT("[ASFireStrike] The target doesn't have an health component. Is the target correct?"));
+			return;
+		}
+
+		const float Current = GetGenericDamageForLevel();
+		const float BurnMultiplier = BurnAttribute->GetValue() * DamagePerBurn;
+		Health->TakeDamage(Current + BurnMultiplier);
+	}
+}
---
+++ Source/CustomUnrealGAS/Public/AbilitySystem/Abilities/ASFireStrike.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ASAbility.h"
+#include "ASFireStrike.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUNREALGAS_API UASFireStrike : public UASAbility
+{
+	GENERATED_BODY()
+
+private:
+	/** The number of damage to add to the total attack damage.	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability|FireStrike", meta = (AllowPrivateAccess = true))
+	float DamagePerBurn = 50;
+	
+public:
+	virtual void UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor) override;
+};
