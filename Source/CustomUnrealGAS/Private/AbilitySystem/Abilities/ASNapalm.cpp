d76c97804f9534c99f1f8715e715622e2fd7816c
TjgL <lithmoneo@gmail.com>
2025-11-02 16:56:15 +0100

    Implemented Napalm ability

---
+++ Source/CustomUnrealGAS/Private/AbilitySystem/Abilities/ASNapalm.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/ASNapalm.h"
+
+#include "ASTargetable.h"
+#include "AbilitySystem/AttributeSystem/ASAttribute.h"
+#include "AbilitySystem/AttributeSystem/ASAttributeSystem.h"
+
+void UASNapalm::UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor)
+{
+	IASTargetable* Target {TargetableActor.GetInterface()};
+	const auto AttributeSystem = Target->GetAttributeSystem();
+	if (const auto BurnAttribute = AttributeSystem->TryGetAttribute(GetGenericData()->Name))
+	{
+		const float Count = BurnAttribute->GetValue();
+		const float Percentage = GetFinalPercentage(GenericDamagePercentage, GenericDamageScalingMode, GenericDamageScaling, Level);
+		BurnAttribute->AddValue(FMath::FloorToInt(Percentage / 100 * Count));
+	}
+}
---
+++ Source/CustomUnrealGAS/Public/AbilitySystem/Abilities/ASNapalm.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ASAbility.h"
+#include "ASNapalm.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUNREALGAS_API UASNapalm : public UASAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor) override;
+};
