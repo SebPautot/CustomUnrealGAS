fba2a9750edb3b33ab124b543e883c9df819e57e
TjgL <lithmoneo@gmail.com>
2025-11-02 17:13:27 +0100

    Implemented Pyromania ability

---
+++ Source/CustomUnrealGAS/Private/AbilitySystem/Abilities/ASPyromania.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ASPyromania.h"
+
+#include "ASHealthComponent.h"
+#include "ASTargetable.h"
+#include "AbilitySystem/AttributeSystem/ASAttribute.h"
+#include "AbilitySystem/AttributeSystem/ASAttributeSystem.h"
+
+void UASPyromania::UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor)
+{
+	IASTargetable* Target {TargetableActor.GetInterface()};
+	if (const auto Health = Target->TryGetHealthComponent(); Health != nullptr)
+	{
+		Health->TakeDamage(GetGenericDamageForLevel());
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(100, 5.f, FColor::Red, TEXT("[ASFireBall] The target doesn't have an health component. Is the target correct?"));
+	}
+
+	const auto AttributeSystem = Target->GetAttributeSystem();
+	if (const auto BurnAttribute = AttributeSystem->GetOrCreateAttribute(*GetGenericData()))
+	{
+		const float Count = BurnToAdd * Level;
+		BurnAttribute->AddValue(Count);
+	}
+}
---
+++ Source/CustomUnrealGAS/Private/AbilitySystem/Abilities/ASPyromania.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/ASAbility.h"
+#include "ASPyromania.generated.h"
+
+/**
+ * Inflicts 50% damage to all targets. Adds 1 (+1) burn to each target.
+ */
+UCLASS()
+class CUSTOMUNREALGAS_API UASPyromania : public UASAbility
+{
+	GENERATED_BODY()
+
+private:
+	/** Number of Burn stacks to add for each target per level of the ability */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Ability|Scaling|Pyromania", meta=(AllowPrivateAccess = true, ClampMin=0))
+	int BurnToAdd = 1;
+
+public:
+	virtual void UseAbilitySingle_Implementation(const TScriptInterface<IASTargetable>& TargetableActor) override;
+};
