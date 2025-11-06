b7ef0ee389239094df06177448210ffdb19f8f4f
TjgL <lithmoneo@gmail.com>
2025-11-06 21:14:51 +0100

    Created base player controller class

---
+++ Source/CustomUnrealGAS/Public/ASPlayerController.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ASPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMUNREALGAS_API AASPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	
+};
